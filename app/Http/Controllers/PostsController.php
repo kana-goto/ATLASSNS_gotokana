3d1b6fc4bc301bdc0c9b793b440e670c4775acc2
kanagoto <tempesta2268@gmail.com>
2022-10-01 14:53:05 +0900

    つぶやきを登録

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth;
 
 class PostsController extends Controller
 {
-    //
     public function index(){
-        return view('posts.index');
+        $list = \DB::table('posts')->get();
+        return view('posts.index',['list'=>$list]);
     }
+
+    public function create(Request $request)
+    {
+        $post = $request->input('newPost');
+        $user_id = auth()->id();
+        \DB::table('posts')->insert([
+            'post' => $post,
+            'user_id' => $user_id
+        ]);
+
+        // $username = Auth::username();
+        // \DB::table('posts')->insert([
+        //     'username' => $user_id
+        // ]);
+
+        // $username = Auth::username();
+        // Post::insert([
+        //     $username => $user_id,
+        //     ]);
+
+        return redirect('/top');
+
+    }
+
 }
