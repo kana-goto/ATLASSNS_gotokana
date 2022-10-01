4bfd2b0a91029f701874e3d65745b46059e8bb76
kanagoto <tempesta2268@gmail.com>
2022-10-01 15:31:25 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::post('/added', 'Auth\RegisterController@added');
 Route::group(['middleware' => 'auth'], function() {
 
 Route::get('/top','PostsController@index');
+Route::post('/top','PostsController@create');
 
 Route::get('/profile','UsersController@profile');
 
